c437236067b6b62f14516fa270fd54696b971dff
sajid43677 <20-43677-2@student.aiub.edu>
2024-02-01 11:25:53 +0600

    Student class added

--- assignment1/assignment1/assignment1/Controllers/HomeController.cs
+++ assignment1/assignment1/assignment1/Controllers/HomeController.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using assignment1.Models;
 
 namespace assignment1.Controllers
 {
     public class HomeController : Controller
     {
+        Student student = new Student();
         public ActionResult Index()
         {
+            
+            student.Name = "Shajid Kamal Joy";
+            student.Email = "20-43677-2@aiub.edu";
+            student.ID = "20-43677-2";
+
+            ViewBag.student = student;
             return View();
         }
 
         public ActionResult Education()
         {
+            Education[] educations = new Education[3];
+            educations[0] = new Education();
+            educations[0].Degree = "SSC";
+            educations[0].Institution = "BN School and College-Chattogram";
+            educations[0].Result = "5.00";
+            educations[0].PassingYear = "2017";
+            educations[1] = new Education();
+            educations[1].Degree = "HSC";
+            educations[1].Institution = "BN School and College-Chattogram";
+            educations[1].Result = "4.42";
+            educations[1].PassingYear = "2019";
+            educations[2] = new Education();
+            educations[2].Degree = "BSc in CSE";
+            educations[2].Institution = "American International University-Bangladesh";
+            educations[2].Result = "3.94";
+            educations[2].PassingYear = "2024";
+            student.Educations = educations;
+            ViewBag.student = student;
             return View();
         }
 
---
+++ assignment1/assignment1/assignment1/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using assignment1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace assignment1.Models
+{
+    public class Education
+    {
+        // Properties
+        public string Degree { get; set; }
+        public string Institution { get; set; }
+        public string Result { get; set; }
+        public string PassingYear { get; set; }
+    }
+    
+        
+    public class Student
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public string ID { get; set; }
+        public Education[] Educations { get; set; }
+        
+
+    }
+}
