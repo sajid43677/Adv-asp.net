b82199af0f096f9ce784acf8fde2f78eb86028b7
sajid43677 <20-43677-2@student.aiub.edu>
2024-03-17 17:58:08 +0600

    mid assignment

--- zerohunger/Controllers/collectorController.cs
+++ zerohunger/Controllers/collectorController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using zerohunger.EF;
 
 namespace zerohunger.Controllers
 {
     public class collectorController : Controller
     {
         // GET: collector
+        ZeroHungerEntities db = new ZeroHungerEntities();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var user = Session["user"] as user;
+            var data = fetch(user.id);
+            return View(data);
+
         }
+
+        [HttpPost]
+        public ActionResult Index(int requestId)
+        {
+            var user = Session["user"] as user;
+            var request = db.requests.FirstOrDefault(x => x.id == requestId);
+            var collector = db.collectors.FirstOrDefault(x => x.id == user.id);
+            request.status = "claimed";
+            collector.current_assigned--;
+            db.SaveChanges();
+            var data = fetch(user.id);
+            return View(data);
+        }
+
+        public static List<request> fetch(int id)
+        {
+            ZeroHungerEntities db = new ZeroHungerEntities();
+            
+            var data = (from r in db.requests
+                        where r.collector.id == id
+                        select r).ToList();
+            return data;
+        }
+
+        
     }
 }
